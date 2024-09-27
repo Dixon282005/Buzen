51ebd0dae002809abbbeb50d6ef20cecabf53c7d
Rafitorcha <vasquezsrafael05@gmail.com>
2024-09-26 21:25:07 -0400

    [ADD] options bar, [FIX] magnifying glass

--- Buzen-front/src/Navbar.jsx
+++ Buzen-front/src/Navbar.jsx
@@ -3,19 +3,28 @@ import './Navbar.css'
 import './colors.css'
 
 const Navbar = () => (
-    <nav>
+
+<div>
+<svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+<section class="options">
+</section>
+<section>
+</section>
+<nav>
+<label for="send">
+<svg class="magnifyingGlass" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+</label>
 <form>
-<img class="lens" color="red" src="search-svgrepo-com.svg"/>
-<button class="lenBehind" type="submit"></button>
+<button id="send" class="send" type="submit"></button>
 <input class="bar" placeholder="Search"/>
 </form>
 <a class="log session" href="lobby">LOG IN</a>  
 <a class="sign session" href="lobby">SIGN UP</a>
-<a class="appLogo">
+<a class="appLogo" href="">
 BU
 </a>  
 </nav>
-
+</div>
 );
 
 export default Navbar
