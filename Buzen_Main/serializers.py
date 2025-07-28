c4fb8e433ac79c9d2ab0702253af79b021d941be
Dixon282005 <dixonramos282005@hotmail.com>
2025-07-28 14:14:04 -0400

    update requirementes

---
+++ Buzen_Main/serializers.py
@@ -0,0 +1,19 @@
+
+from rest_framework import serializers
+from .models import CustomUser 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password', 'avatar']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            avatar=validated_data.get('avatar')
+        )
+        return user
