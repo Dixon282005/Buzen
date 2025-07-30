0078eb7c4721b71802e13ba3bc6abfd885fb3d12
Dixon282005 <dixonramos282005@hotmail.com>
2025-07-29 22:07:38 -0400

    Navbar styles

--- Buzen-front/src/components/Header.jsx
+++ Buzen-front/src/components/Header.jsx
@@ -1,49 +1,49 @@
 import { useState } from 'react'
 
 function Nav() {
-  return (
-    <header className="sticky top-4 z-[1000] flex justify-center pointer-events-none">
-      <nav className="w-full max-w-7xl flex justify-between items-center py-3 px-8 bg-white/80 backdrop-blur-md  pointer-events-auto transition duration-300">
-        
-        {/* Izquierda: Logo + search */}
-        <div className="flex items-center gap-6">
-          <a href="/">
-            <img src="" alt="Logo" className="h-8 w-auto" />
-          </a>
+    return (
+        <header className="sticky top-4 z-[1000] flex justify-center pointer-events-none">
+            <nav className="w-full max-w-7xl flex justify-between items-center py-3 px-8 bg-white/80 backdrop-blur-md  pointer-events-auto transition duration-300">
 
-          <input 
-            type="search" 
-            placeholder="Search Music..." 
-            className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </div>
 
-        {/* Derecha: Menú de navegación */}
-        <ul className="flex gap-6 list-none m-0 p-0">
-          <li>
-            <a href="#" className="font-semibold text-base px-6 py-2 rounded-md hover:text-accent hover:bg-white/10 transition-colors duration-300">
-              Sign in
-            </a>
-          </li>
-          <li>
-            <a href="#" className="font-semibold text-base px-6 py-2 rounded-md hover:text-accent hover:bg-white/10 transition-colors duration-300">
-              Sign Up
-            </a>
-          </li>
-          <li>
-            <a href="#" className="font-semibold text-base px-6 py-2 rounded-md hover:text-accent hover:bg-white/10 transition-colors duration-300">
-              Premium
-            </a>
-          </li>
-          <li>
-            <a href="#" className="font-semibold text-base px-6 py-2 rounded-md hover:text-accent hover:bg-white/10 transition-colors duration-300">
-              Home
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+                <div className="flex items-center gap-6">
+                    <a href="/">
+                        <img src="./public/Buzen-logo.png" alt="Logo" className=" h-20 w-auto" />
+                    </a>
+
+                    <input
+                        type="search"
+                        placeholder="Search Music..."
+                        className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    />
+                </div>
+
+                {/* Derecha: Menú de navegación */}
+                <ul className="flex gap-6 list-none m-0 p-0">
+                    <li>
+                        <a href="#" className="inline-block font-semibold text-base px-6 py-2 rounded-md  text-gray-600 hover:bg-amber-500 transform hover:-translate-y-[3px] transition duration-300">
+                            Sign in
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="inline-block font-semibold text-base px-6 py-2 rounded-md text-gray-600 hover:bg-amber-500 transform hover:-translate-y-[3px] transition duration-300">
+                            Sign Up
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="inline-block font-semibold text-base px-6 py-2 rounded-md  text-gray-600 hover:bg-amber-500 transform hover:-translate-y-[3px] transition duration-300">
+                            Premium
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="inline-block font-semibold text-base px-6 py-2 rounded-md  text-gray-600 hover:bg-amber-500 transform hover:-translate-y-[3px] transition duration-300">
+                            Home
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Nav;
